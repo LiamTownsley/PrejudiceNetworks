028c882cc5480f32ad973464215fb8754544c82f
Liam Townsley <gitkraken@liamtownsley.me>
2020-06-21 21:49:53 +0100

    Addition of custom logging

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const prefix = process.env.prefix
 const Discord = require('discord.js');
 
 const { success } = require('log-symbols');
+const { log } = require('./modules/logger')
 const fs = require('fs');
 const bot = new Discord.Client();
       bot.commands = new Discord.Collection();
@@ -14,12 +15,14 @@ const bot = new Discord.Client();
           "721281052974252043": 3, // Staff
           "722489210711572670": 4, // Leadership
           "722458984690221078": 5, // Discord Permissions
-      }
+        }
+      bot.log = log
+      
 
 fs.readdirSync('./commands/').forEach(dirs => {
     const commands = fs.readdirSync(`./commands/${dirs}/`).filter(files => files.endsWith(".js"));
     for (const file of commands) {
-        console.log(success, `Loading Command \x1b[36m${file}\x1b[0m in module \x1b[36m${dirs}\x1b[0m`)
+        bot.log(`${success} Loading Command \x1b[36m${file}\x1b[0m in module \x1b[36m${dirs}\x1b[0m`)
         const command = require(`./commands/${dirs}/${file}`);
         bot.commands.set(command.name, command);
     }
@@ -29,7 +32,7 @@ fs.readdir(`./events/`, (err, files) => {
     console.log("")
     if (err) return console.error(err);
     files.forEach(file => {
-        console.log(success, `Loading Event: \x1b[36m${file}\x1b[0m`)
+        bot.log(`${success} Loading Event: \x1b[36m${file}\x1b[0m`)
         const event = require(`./events/${file}`);
         let eventName = file.split(".")[0];
         bot.on(eventName, event.bind(null, bot));
--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,9 @@ module.exports = (bot) => {
         type: 'WATCHING'
     });
 
-    console.log(`The bot is now \x1b[36mready\x1b[0m. View some information below:\nStatus: \x1b[36mWatching Prejudice Networks\x1b[0m\nTotal Guild Members: \x1b[36m${bot.users.cache.array().length}\x1b[0m`)
+    bot.log(`The bot is now \x1b[36mready\x1b[0m. View some information below:`)
+    bot.log(`Status: \x1b[36mWatching Prejudice Networks\x1b[0m`)
+    bot.log(`Total Guild Members: \x1b[36m${bot.users.cache.array().length}\x1b[0m`)
     const guild = bot.guilds.cache.get("721112680848556034");
     function updateMembers() {
         var memberCount = guild.members.cache.filter(member => !member.user.bot).size;
@@ -25,6 +27,8 @@ module.exports = (bot) => {
     updateBans()
     setInterval(updateMembers, 60000 * 5);
     setInterval(updateBans, 60000 * 5);
-
+    /*setInterval(() => {
+        bot.log("test", "WARN")
+    }, 1000);*/
 }
 
